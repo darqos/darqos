671699a15040a153b66dfde5bc253dd1645f8273
David Arnold <d@0x1.org>
2020-07-17 06:18:14 +0000

    Updated to include history service.

--- boot.sh
+++ boot.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+export PYTHONPATH=/Users/d/work/personal/darqos
+
 # Start services.
-python services/storage.py &
-python services/ui/ui.py &
+python services/storage/main.py &
+python services/history/main.py &
+
+python services/terminal/main.py &
 
 
