174fed643553e1819ee6ff681e8cb8c154d94cdf
David Arnold <d@0x1.org>
2022-03-22 12:31:22 +0000

    Skeleton Python boot.
    Note that current procedure is to use top-level boot.sh script, not
    this file.

--- darq/os/boot.py
+++ darq/os/boot.py
@@ -1,2 +1,27 @@
 #
 
+import os
+
+
+def boot():
+    """Boot the M0 interim implementation."""
+
+    # Service registry / namespace / IPC thing.
+
+    # Storage service.
+    os.spawnl()
+
+    # Metadata service.
+
+    # Index service.
+
+    # Security service.
+
+    return
+
+
+def shutdown():
+    """Perform an orderly shutdown of the M0 implementation."""
+
+    # reverse order shutdown of services.
+    return
