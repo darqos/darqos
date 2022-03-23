050e22f97f1ccea4f57be11b38af6cbba3a4edd3
David Arnold <d@0x1.org>
2022-03-23 14:50:15 +0000

    Placeholder for process registry.

--- darq/os/kernel.py
+++ darq/os/kernel.py
@@ -7,3 +7,16 @@ class Kernel:
     def __init__(self):
         return
 
+
+    def register_process(self, pid):
+        """Register an externally-created process.
+
+        For now, all darq processes are actually Unix processes, and in
+        some cases, they're created outside of darq's 'system calls'.
+        In order to enable darq to manage these processes, they need to
+        be registered, using this function."""
+
+        # FIXME!
+        pass
+
+
