a46b7d28889b96389c0f4e859a6729009aa0c2df
David Arnold <d@0x1.org>
2022-06-06 05:20:11 +0000

    Begin to move boot process into Python.

--- darq/os/boot.py
+++ darq/os/boot.py
@@ -1,21 +1,37 @@
 #
 
 import os
+import sys
+
+BOOT_ERR = f"""Error: unable to find DarqOS system files in %s.
+You must either start from the DarqOS installation directory, or set 
+the DARQ_ROOT environment variable to point to it.
+"""
 
 
 def boot():
     """Boot the M0 interim implementation."""
 
-    # Service registry / namespace / IPC thing.
+    # Look for the system files.
+    root = os.getenv("DARQ_ROOT")
+    if root is None:
+        root = os.getcwd()
 
-    # Storage service.
-    os.spawnl()
+    if not os.path.exists(f"{root}/darq/rt/type.py"):
+        sys.stderr.write(BOOT_ERR % root)
+        sys.exit(1)
 
-    # Metadata service.
+    # Make sure system root is first in Python's import path.
+    python_path = os.getenv("PYTHONPATH")
+    if python_path is None:
+        os.environ["PYTHONPATH"] = root
+    else:
+        os.environ["PYTHONPATH"] = f"{root}:{os.getenv('PYTHONPATH')}"
 
-    # Index service.
+    # FIXME: no dependencies for now: just use fixed order.
 
-    # Security service.
+    storage_pid = os.spawnv(os.P_NOWAIT, "python", [f"{root}/services/storage/main.py"])
+    terminal_pid = os.spawnv(os.P_NOWAIT, "python", [f"{root}/services/terminal/main.py"])
 
     return
 
