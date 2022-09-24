2891c80a66c64bc6fba76673fc583cc6e8b52331
David Arnold <d@0x1.org>
2022-09-24 23:17:03 +0000

    Placeholder for tool process base class.

---
+++ darq/runtime/tool.py
@@ -0,0 +1,10 @@
+# darqos
+# Copyright (C) 2022 David Arnold
+
+# Tools are application processes that are not a type implementation.
+
+from darq import PortListener
+
+
+class Tool(PortListener):
+    pass
