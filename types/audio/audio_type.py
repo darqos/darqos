7e63598cd1f87d6f7585e183d2d3cc393681875d
David Arnold <d@0x1.org>
2023-05-02 14:52:10 +0000

    Placeholder audio type.

---
+++ types/audio/audio_type.py
@@ -0,0 +1,14 @@
+# DarqOS
+# Copyright (C) 2023 David Arnold
+
+class AudioType:
+    """Implements the functionality of the "audio" type."""
+    pass
+
+class AudioTypeImplementation:
+    """Implements the system interface to the audio type."""
+    pass
+
+class AudioQtLens:
+    """Implements a Qt GUI lens for audio type instances."""
+    pass
