28b70b164989adccb7b485811ea5192b80fd9d6f
David Arnold <d@0x1.org>
2023-05-02 14:52:53 +0000

    Placeholder filesystem type.

---
+++ types/filesystem/filesystem_type.py
@@ -0,0 +1,29 @@
+# DarqOS
+# Copyright (C) 2023, David Arnold
+
+
+class File:
+    """Implements the functionality of a "file" object."""
+    pass
+
+
+class FileType:
+    """Implements the system interface to a file type"""
+    pass
+
+
+class FileQtLens:
+    """Implements the Qt GUI lens for a file object."""
+    pass
+
+
+class Filesystem:
+    pass
+
+
+class FilesystemType:
+    pass
+
+
+class FilesystemQtLens:
+    pass
